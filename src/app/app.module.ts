44031440b8dc06e049c9b13be8f287391d9c615d
soumyaRauth <shoummorauth@github.com>
2020-03-08 01:30:38 +0600

    header created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-item/recipe-item.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { HeaderComponent } from './header/header.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    RecipeListComponent,
+    HeaderComponent,
+    RecipesComponent
+
   ],
   imports: [
     BrowserModule
