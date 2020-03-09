ec38c4557af08cb764867e399328e4f4d0ff5a69
soumyaRauth <shoummorauth@github.com>
2020-03-09 21:53:35 +0600

    recipe item added with dummy content

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Recipe } from '../recipe.model';
+
 
 @Component({
-    selector:'app-recipe-list',
-    templateUrl:'./recipe-list.component.html',
-    styleUrls:['./recipe-list.component.scss']
+  selector: "app-recipe-list",
+  templateUrl: "./recipe-list.component.html",
+  styleUrls: ["./recipe-list.component.scss"]
 })
-export class RecipeListComponent{
+export class RecipeListComponent implements OnInit {
+    recipes: Recipe[] = [
+        new Recipe("chow mein", "Chinese Chow min", "https://c.ndtvimg.com/mnng9ei8_chowmein_640x480_25_July_18.jpg")
+      ];
+    
 
+  ngOnInit() {}
 }
